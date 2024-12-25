82d4746f0ad6c53d000740b8a35fb0eaf50a56d1
liupengsay <liupengsay@foxmail.com>
2024-12-26 00:11:32 +0800

    课程学习

--- pytorch_learning/src/test.py
+++ pytorch_learning/src/test.py
@@ -1,13 +1,22 @@
-from PIL import Image
-import numpy as np
+# from PIL import Image
+# import numpy as np
+#
+# # 创建一个 32x32x3 的矩阵作为示例数据
+# matrix = np.random.randint(0, 256, size=(32, 32, 3), dtype=np.uint8)
+#
+# # 将矩阵转换为 PIL Image 对象
+# image = Image.fromarray(matrix)
+#
+# # 保存图片
+# image.save('output.png')
+#
+# print("Image saved as output.png")
 
-# 创建一个 32x32x3 的矩阵作为示例数据
-matrix = np.random.randint(0, 256, size=(32, 32, 3), dtype=np.uint8)
-
-# 将矩阵转换为 PIL Image 对象
-image = Image.fromarray(matrix)
-
-# 保存图片
-image.save('output.png')
-
-print("Image saved as output.png")
+st = ""
+for x in range(1200, 1400):
+    st += chr(x)
+    print(chr(x))
+print(st)
+st1 = "liupengsay"
+st2 = "ӏiupengsay"
+st = "ԔԕԖԗԘԙԚԛԜԝԞԟԠԡԢԣԤԥԦԧԨԩԪԫԬԭԮԯ԰ԱԲԳԴԵԶԷԸԹԺԻԼԽԾԿՀՁՂՃՄՅՆՇՈՉՊՋՌՍՎՏՐՑՒՓՔՕՖ՗՘ՙ՚՛՜՝՞՟ՠաբգդեզէըթժիլխծկհձղճմյնշ"
